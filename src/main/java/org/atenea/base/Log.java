befbecf0acef9e509ee331dfb7ff771f9720a877
ric <ric@localhost>
2013-05-23 23:20:27 -0300

    Cambio POM para compilar y saco unas lineas de Log.java para que no tire warning

--- src/main/java/org/atenea/base/Log.java
+++ src/main/java/org/atenea/base/Log.java
@@ -1,18 +1,8 @@
 package org.atenea.base;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Log {
 	
-	private Logger logger;
 	
-	
-	public void startLogger(Object obj){
-		
-		logger = LogManager.getLogger(this.getClass());
-		
-	}
 	
 	
 }
