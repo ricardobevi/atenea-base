5b23ad3090ca91174cb8b9b412914643a7f6c9be
ric <ric@localhost>
2013-08-07 17:14:06 -0300

    Agrego metodos a grafo

--- src/main/java/org/squadra/atenea/base/graph/Graph.java
+++ src/main/java/org/squadra/atenea/base/graph/Graph.java
@@ -2,6 +2,8 @@ package org.squadra.atenea.base.graph;
 
 import java.util.HashMap;
 
+import com.google.gson.Gson;
+
 
 public class Graph<T> {
 
@@ -13,11 +15,17 @@ public class Graph<T> {
 	}
 	
 	public void addNode( Node<T> node ){
+		node.setId(graph.size());
 		graph.put( graph.size(), node );
 	}
 	
 	public void addNode(Node<T> node, Integer index){
+		
+		if ( !graph.containsKey(index) ){
+			node.setId(index);
 			graph.put( index, node );
+		}
+		
 	}
 	
 	public Node<T> getNode(Integer index){
@@ -26,6 +34,15 @@ public class Graph<T> {
 	
 	public void relate(Integer index1, Integer index2){
 		graph.get(index1).relate(index2);
+	}
+	
+	public String toString() {
+		Gson gson = new Gson();
+		String ret = "";
+
+		ret = gson.toJson(this);
+
+		return ret;
 	}
 	
 }
