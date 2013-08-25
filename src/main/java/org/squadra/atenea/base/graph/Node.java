be7606ed94f8c8e50df12705cab9c7f9bc401586
lea <lea@localhost>
2013-08-25 17:58:04 -0300

    Clase Graph: agrego metodo subgrafo, elimino las relaciones y las dejo solo en los nodos, agrego calculo de distancia al nodo raiz. Clase Node: Separo las relaciones en relaciones de entrada y salida. Agrego comentarios.

--- src/main/java/org/squadra/atenea/base/graph/Node.java
+++ src/main/java/org/squadra/atenea/base/graph/Node.java
@@ -1,38 +1,51 @@
 package org.squadra.atenea.base.graph;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 
 import lombok.Data;
 
+/**
+ * Clase nodo que se utiliza en la clase Graph.
+ * @author Ricardo Bevilacqua
+ * @author Leandro Morrone
+ *
+ * @param <T> tipo de nodo
+ */
 public @Data class Node<T> {
 	
 	private Integer id;
 	private T data;
-	private HashSet< Integer > relatedTo;
+	private ArrayList< Integer > inputRelations;
+	private ArrayList< Integer > outputRelations;
 
 	
 	public Node(T data){
 		this.data = data;
-		relatedTo = new HashSet< Integer >();
+		inputRelations = new ArrayList< Integer >();
+		outputRelations = new ArrayList< Integer >();
 	}
 	
-	public Boolean isRelatedTo( Integer nodeIndex ){
-		return relatedTo.contains(nodeIndex);
+	public Node(Node<T> newNode){
+		id = newNode.id;
+		data = newNode.data;
+		inputRelations = newNode.inputRelations;
+		outputRelations = newNode.outputRelations;
 	}
 	
-	
-	public void relate( Integer nodeIndex ){
-		relatedTo.add(nodeIndex);
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
+	public Boolean isInputRelatedTo( Integer nodeIndex ){
+		return inputRelations.contains(nodeIndex);
 	}
 	
+	public Boolean isOutputRelatedTo( Integer nodeIndex ){
+		return outputRelations.contains(nodeIndex);
+	}
 	
+	public void inputRelate( Integer nodeIndex ){
+		inputRelations.add(nodeIndex);
+	}
+	
+	public void outputRelate( Integer nodeIndex ){
+		outputRelations.add(nodeIndex);
+	}
 
 }
