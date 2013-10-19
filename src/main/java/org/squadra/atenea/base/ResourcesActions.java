f228386495f1a3a721ef3b61b9224c20dbea2ef8
lucas <lucas@localhost>
2013-10-19 13:40:09 -0300

    Se cambian los paths re ResourcesActions

--- src/main/java/org/squadra/atenea/base/ResourcesActions.java
+++ src/main/java/org/squadra/atenea/base/ResourcesActions.java
@@ -13,22 +13,18 @@ import javax.swing.ImageIcon;
  */
 public class ResourcesActions {
 
-	private static final String BASE_PATH = new String().getClass().getResource("/media/").getPath();
+	public static final String BASE_PATH = new String().getClass().getResource("/").getPath();
 
 	public static class Actions {
 			
-		public static final String PATH = BASE_PATH + "actions/";
+		public static final String PATH = BASE_PATH ;
 		
 		public static final String actions_file = PATH + "actions.json";
 		public static final String commands_file = PATH + "commands.txt";
 		public static final String screenshot_file = PATH + "screenshot.jpg";
 		public static final String result_file = PATH + "result2.jpg";
 		public static final String output_command_file = PATH + "output_command.txt";
-		public static final String ICONS_PATH = PATH + "images/";
-	}
-	
-	public static enum Colors {
-		GREEN, RED, BLUE, ORANGE, YELLOW, GREY
+		public static final String ICONS_PATH = PATH ;
 	}
 	
 }
