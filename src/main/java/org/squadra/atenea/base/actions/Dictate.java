e6af6ee9597d95ff0b694639a02ee43287c17ae4
lucas <lucas@localhost>
2013-10-26 02:22:40 -0300

    Se corrige la accion de dictado para que escriba correctamente vocales con tilde. Falta reconocer la letra '?'

--- src/main/java/org/squadra/atenea/base/actions/Dictate.java
+++ src/main/java/org/squadra/atenea/base/actions/Dictate.java
@@ -1,5 +1,6 @@
 package org.squadra.atenea.base.actions;
 
+import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.event.*;
@@ -10,56 +11,118 @@ public class Dictate extends PreloadAction {
 	public void execute() {
 		//Hago click en la mitad de la pantalla para darle foco a lo que esté atras -.-'
 		try {
-			
+
 			Robot r = new Robot();
 			r.mouseMove(Toolkit.getDefaultToolkit().getScreenSize().width / 2,
 					Toolkit.getDefaultToolkit().getScreenSize().height / 2);
 			r.mousePress(InputEvent.BUTTON1_MASK);
 			r.mouseRelease(InputEvent.BUTTON1_MASK);
 		} catch (Exception e) {		}
-		
+
 		//Escribo
 		typeString(param);
 	}
 
 	public void typeString(String s) {
+
+		Robot robik = null;
 		try {
-			Robot robik = new Robot();
-			byte[] bytes = s.getBytes();
-			System.out.println(bytes);
-			for (byte b : bytes) {
-				int code = b;
-				pressUnicode(robik, code);
-//				// Si la letra es mayuscula, apreto Shift
-//				if (code > 64 && code < 91)
-//					robik.keyPress(KeyEvent.VK_SHIFT);
-//				if (code > 96 && code < 123) 
-//					code -= 32;		
-//				
-//				robik.delay(40);
-//				robik.keyPress(code);
-//				robik.keyRelease(code);
-//				// Si la letra es mayuscula, suelto Shift
-//				if (code > 64 && code < 91)
-//					robik.keyRelease(KeyEvent.VK_SHIFT);
+			robik = new Robot();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] bytes = s.getBytes();
+		for (byte b : bytes) {
+			int code = b;
+			System.out.println("Code:" + code);
+			//				// Si la letra es mayuscula, apreto Shift
+			if (code > 64 && code < 91)
+				robik.keyPress(KeyEvent.VK_SHIFT);
+			if (code > 96 && code < 123) 
+				code -= 32;		
+
+			robik.delay(40);
+
+			if (code == -95)
+			{					
+				robik.keyPress( KeyEvent.VK_DEAD_ACUTE);
+
+				robik.keyPress(65);
+				robik.keyRelease(65);					
+				robik.keyRelease( KeyEvent.VK_DEAD_ACUTE);
+
 			}
-		} catch (Exception e) {}
+			else if (code == -83)
+			{				
+				robik.keyPress( KeyEvent.VK_DEAD_ACUTE);
+
+				robik.keyPress(73);
+				robik.keyRelease(73);			
+				robik.keyRelease( KeyEvent.VK_DEAD_ACUTE);
+
+			}
+			else if (code == -77)//162)
+			{					
+				robik.keyPress( KeyEvent.VK_DEAD_ACUTE);
+
+				robik.keyPress(79);
+				robik.keyRelease(79);				
+				robik.keyRelease( KeyEvent.VK_DEAD_ACUTE);
+
+			}
+			else if (code == -70)
+			{				
+				robik.keyPress( KeyEvent.VK_DEAD_ACUTE);
+
+				robik.keyPress(85);
+				robik.keyRelease(85);				
+				robik.keyRelease( KeyEvent.VK_DEAD_ACUTE);
+
+			}
+			else if (code == -87)
+			{					
+				robik.keyPress( KeyEvent.VK_DEAD_ACUTE);
+
+				robik.keyPress(69);
+				robik.keyRelease(69);					
+				robik.keyRelease( KeyEvent.VK_DEAD_ACUTE);
+
+			}
+			// ñ
+			else if (code == -79)
+			{
+				robik.keyPress(KeyEvent.VK_N);
+				robik.keyRelease(KeyEvent.VK_N);					
+
+			}
+			else if (code > 0)
+			{
+				robik.keyPress(code);
+				robik.keyRelease(code);
+			}
+			// Si la letra es mayuscula, suelto Shift
+			if (code > 64 && code < 91)
+				robik.keyRelease(KeyEvent.VK_SHIFT);
+
+		}
+
 	}
-	
+
 	public static void pressUnicode(Robot r, int key_code)
 	{
-	    r.keyPress(KeyEvent.VK_ALT);
+		r.keyPress(KeyEvent.VK_ALT);
 
-	    for(int i = 3; i >= 0; --i)
-	    {
-	        // extracts a single decade of the key-code and adds
-	        // an offset to get the required VK_NUMPAD key-code
-	        int numpad_kc = key_code / (int) (Math.pow(10, i)) % 10 + KeyEvent.VK_NUMPAD0;
+		for(int i = 3; i >= 0; --i)
+		{
+			// extracts a single decade of the key-code and adds
+			// an offset to get the required VK_NUMPAD key-code
+			int numpad_kc = key_code / (int) (Math.pow(10, i)) % 10 + KeyEvent.VK_NUMPAD0;
 
-	        r.keyPress(numpad_kc);
-	        r.keyRelease(numpad_kc);
-	    }
+			r.keyPress(numpad_kc);
+			r.keyRelease(numpad_kc);
+		}
 
-	    r.keyRelease(KeyEvent.VK_ALT);
+		r.keyRelease(KeyEvent.VK_ALT);
 	}
 }
